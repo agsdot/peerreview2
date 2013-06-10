1761a5697492a598e28737df7be436bff0b544ae
Jonathan Lai <jonathanlai@eltux.net>
2013-06-10 14:52:34 -0700

    session controller and user.rb model changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ActiveRecord::Base
   attr_accessible :classroom_admin, :name, :provider, :ui
+
+  def self.from_omniauth(auth)
+    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+  end
+
+  def self.create_from_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["nickname"]
+    end
+  end
+
+
 end
+
+
